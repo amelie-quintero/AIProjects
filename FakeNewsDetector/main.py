5013ea2f2fd3e2266d404d2111579975abb0d558
amelie-quintero <rosepetalbouquet@proton.me>
2026-01-20 16:12:58 -0500

    import training data and preprocess/encode in main

--- FakeNewsDetector/main.py
+++ FakeNewsDetector/main.py
@@ -6,3 +6,10 @@ from sklearn import preprocessing
 from keras.layers import TextVectorization
 from keras.utils import pad_sequences
 
+training_data = pd.read_csv('data/training.csv')
+
+training_data = training_data.drop(['Unnamed: 0'], axis=1)
+
+le = preprocessing.LabelEncoder()
+le.fit(training_data['label'])
+training_data['label'] = le.transform(training_data['label'])
