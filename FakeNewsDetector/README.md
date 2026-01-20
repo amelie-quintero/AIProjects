fa8805ac9a03e957907cead9129f57e284fa97ac
amelie-quintero <rosepetalbouquet@proton.me>
2026-01-20 16:07:11 -0500

    imports updated for forward-compatibility

---
+++ FakeNewsDetector/README.md
@@ -0,0 +1,4 @@
+This project is based on the project in https://www.geeksforgeeks.org/nlp/fake-news-detection-model-using-tensorflow-in-python/
+
+The goal of this project is to create a fake news detection algorithm using TensorFlow in Python.
+
---
+++ FakeNewsDetector/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from keras.layers import TextVectorization
+from keras.utils import pad_sequences
+
